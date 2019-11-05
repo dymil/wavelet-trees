324329041fd8cbdce5feb68b671ba1c16b4bcf9c
dymil <30931139+dymil@users.noreply.github.com>
2019-11-05 13:37:49 -0500

    Fix all the bugs in rank

--- rank_support.ml
+++ rank_support.ml
@@ -1,22 +1,22 @@
 open Bitv
    
 class rank_support bv =
-  let log2 a = log (float_of_int a) /. log 2. in
+  let log2 a = log (float_of_int a) /. log 2.0 in
   let log2_n = log2 @@ length bv in
   let x = int_of_float log2_n in
-  (* do logs early so that s/b = int(log2 n) or 1 + int(log2 n)) is nice *)
+  (* do logs early so that s/b = int(log2 n) or int(log2 n) - 1) is nice *)
   let s = x * x / 2 and
-      b = x / 2 and
+      b = x / 2 (*int_of_float @@ ceil @@ log2_n /. 2.0*) and
       bv_get_chunk arr idx len =
         to_int_us @@ (* should be constant-ish time *)
-          let arr' = Printf.printf "bvlen=%d, idx=%d, size=%d\n" (length arr) idx len; (sub arr idx Sys.int_size) in
+          let arr' = (*Printf.printf "bvlen=%d, idx=%d, size=%d\n" (length arr) idx len;*) (sub arr idx Sys.int_size) in
           fill arr' len (Sys.int_size - len) false; arr' in
   let bv_get_int arr idx size =
     bv_get_chunk arr (idx * size) size and
       bv_get_bit arr idx = if get arr idx then 1 else 0 and
       bv_set_int v arr idx size = blit (of_int_us v) 0 arr (idx * size) size in
   let r_s_el_size = int_of_float @@ ceil @@ log2_n and
-      r_b_el_size = int_of_float @@ ceil @@ log2 @@ s and
+      r_b_el_size = int_of_float @@ 0.1 +. (ceil @@ log2 @@ s) and
       r_p_el_size = int_of_float @@ ceil @@ log2 @@ b + 1 in
   object(self)
     val r_s =
@@ -28,7 +28,7 @@ class rank_support bv =
                               bv_get_int arr (j-1) r_s_el_size else
                               rank (k-1)
            in rank @@ j*s - 1 in
-         Printf.printf "%d:%d " j cum_rank;
+         (*Printf.printf "%d:%d " j cum_rank;*)
          bv_set_int cum_rank arr j r_s_el_size
        done; arr)
     val r_b =
@@ -36,10 +36,10 @@ class rank_support bv =
       let arr = create (dim * r_b_el_size + max 0 (Sys.int_size - r_b_el_size)) false in
       (for j = 1 to dim - 1 do
          let local_rank =
-           let rec rank k = if k mod (s/b) = 0 then bv_get_bit bv k else
-                              if k = (j - 1) * b then bv_get_int arr (j-1) r_b_el_size
-                              else rank (k-1) + bv_get_bit bv k in
-           rank @@ j*b - 1 in
+           let rec rank k = if k mod s = 0 then 0 else
+                              if k = (j-1) * b then bv_get_int arr (j-1) r_b_el_size
+                              else rank (k-1) + (bv_get_bit bv @@ k-1) in
+           rank @@ j*b in
          bv_set_int local_rank arr j r_b_el_size
        done; arr)
         (* note r_p is done in little endian order as opposed to the examples *)
@@ -47,7 +47,7 @@ class rank_support bv =
       let dim1 = 1 lsl b and
           dim2 = b in
       let arr = create (dim1 * dim2 * r_p_el_size + max 0 (Sys.int_size - r_p_el_size)) false in
-      let () = Printf.printf "dim1=%d, dim2=%d length=%d\n" dim1 dim2 @@ length arr in
+      (*Printf.printf "dim1=%d, dim2=%d length=%d\n" dim1 dim2 @@ length arr;*)
       (for i = 1 to dim1 - 1 do
          bv_set_int (i mod 2) arr (i * dim2) r_p_el_size;
          for j = 1 to dim2 - 1 do
@@ -56,12 +56,16 @@ class rank_support bv =
        done; arr)
     (*i is 1-indexed; rank is inclusive here*)
     method rank1 i =
-      let bv' = append bv @@ create (Sys.int_size - 1) false in
-      let block_idx = (i - 1) / b in
-      let r_s_comp = bv_get_int r_s ((i - 1) / s) r_s_el_size and
+      let (*print_bv name bv size dim = Printf.printf "%s " name; for i = 0 to dim - 1 do
+                          Printf.printf "%d " @@ bv_get_int bv i size
+                        done; Printf.printf "\n" and*)
+          bv' = append bv @@ create (Sys.int_size - 1) false in
+      (*print_bv "r_s" r_s r_s_el_size ((s - 1 + length bv) / s) ; print_bv "r_b" r_b r_b_el_size ((b - 1 + length bv) / b); print_bv "r_p" r_p r_p_el_size ((1 lsl b) * b);*)
+      let block_idx = i / b in
+      let r_s_comp = bv_get_int r_s (i / s) r_s_el_size and
           r_b_comp = bv_get_int r_b block_idx r_b_el_size and
-          r_p_comp = bv_get_int r_p (bv_get_chunk bv' (block_idx * b) (i - block_idx * b)) r_p_el_size in
-      Printf.printf "rank1: r_s: s=%d, comp=%d; r_b: b=%d, comp=%d; r_p: l=%d, comp=%d block_idx=%d\n" s r_s_comp b r_b_comp (length r_p) r_p_comp block_idx;
+          r_p_comp = bv_get_int r_p (i - block_idx * b + b * (bv_get_chunk bv' (block_idx * b) (max 0 @@ i - block_idx * b))) r_p_el_size in
+      (*Printf.printf "rank1: r_s: s=%d, comp=%d; r_b: b=%d, comp=%d; r_p: l=%d, comp=%d block_idx=%d\n" s r_s_comp b r_b_comp (length r_p) r_p_comp block_idx;*)
       r_s_comp + r_b_comp + r_p_comp
     method rank0 i = i - (self#rank1 i)
     method overhead = length r_s + length r_b + length r_p
--- test_rank.ml
+++ test_rank.ml
@@ -1,11 +1,11 @@
 let () = Random.self_init () in
     let bv_get_bit arr idx = if (Bitv.get arr idx) then 1 else 0 and
-        n_ops = 1 in
-    let sizes = [(*1; 2;*)4; 8; (*16; 128; 256; 31; 33; 62; 63; 65*)] in
-    let bvs = [(*Bitv.of_int_us @@ Random.bits ();
+        n_ops = 1000 in
+    let sizes = [(*1; 2; *)4; 8; 16; 128; 256; 31; 33; 65] in
+    let bvs = [Bitv.of_int_us @@ Random.bits ();
                Bitv.of_int32_us @@ Random.int32 @@ Int32.max_int;
                Bitv.of_nativeint_us @@ Random.nativeint @@ Nativeint.max_int;
-               Bitv.of_int64_us @@ Random.int64 Int64.max_int*)]
+               Bitv.of_int64_us @@ Random.int64 Int64.max_int]
               @ List.map (fun n ->
                     Bitv.of_list_with_length (
                         let rec f k l =
@@ -16,17 +16,17 @@ let () = Random.self_init () in
       let rec rank_h j =
         if j = -1 then 0 else (bv_get_bit bv j) + (rank_h @@ j - 1) in
       rank_h @@ i-1 in
-    let naives = List.map (fun bv -> let rands = Array.init n_ops @@ (fun x -> let rand = Random.int @@ Bitv.length bv in (rand, naive_rank bv rand)) in
+    let naives = List.map (fun bv -> let rands = Array.init n_ops @@ (fun x -> let rand = 1 + (Random.int @@ Bitv.length bv - 1) in (rand, naive_rank bv rand)) in
                                      (bv, rands)) bvs in
     List.iter (fun (bv, rands)  ->
-        Printf.printf "\n"; for j = 0 to Bitv.length bv - 1 do
+        (*Printf.printf "\n"; for j = 0 to Bitv.length bv - 1 do
           Printf.printf "%d" (if (Bitv.get bv j) then 1 else 0)
-        done; Printf.printf "\n";
+        done; Printf.printf "\n";*)
         let r = new Rank_support.rank_support bv in
         let start = Unix.gettimeofday() in
         for j = 0 to n_ops - 1 do
-          Printf.printf "len=%d, i=%d\n" (Bitv.length bv) @@ fst rands.(j);
+          (*Printf.printf "len=%d, i=%d\n" (Bitv.length bv) @@ fst rands.(j);*)
           let res = r#rank1 @@ fst rands.(j) in
           OUnit2.assert_equal ~printer:string_of_int (snd rands.(j)) res
-        done; Printf.printf "Execution time %f sec, overhead %d bits" (Unix.gettimeofday() -. start) r#overhead
+        done; Printf.printf "Bitv length: %d, Execution time: %f sec, overhead: %d bits\n" (Bitv.length bv) (Unix.gettimeofday() -. start) r#overhead
       ) naives
